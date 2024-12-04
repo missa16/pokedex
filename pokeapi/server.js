bb4a4ab8a81b24152091243f3c1ab10a2d335cea
rberr <rberrekam@gmail.com>
2024-12-04 11:51:49 +0100

    modif

--- pokeapi/server.js
+++ pokeapi/server.js
@@ -2,9 +2,17 @@ import Fastify from 'fastify'
 import fastifySensible from '@fastify/sensible'
 import pokemons from './routes/pokemons.js';
 
+
+const HEALTHCHECK_ENDPOINT = process.env.HEALTHCHECK_ENDPOINT || "/health"; //toujours bien d'avoir des valeurs par defaut
+const POKEDEX_NAME = process.env.POKEDEX_NAME;
+if(typeof POKEDEX_NAME === undefined){
+    throw new Error('POKEDEX_NAME must be set');
+}
+
 const fastify = Fastify({
     logger: true
 });
+
 
 // plugins
 fastify.register(fastifySensible);
@@ -15,6 +23,15 @@ fastify.get('/', (request, reply) => {
         .type('text/html')
         .send(`<h1>Welcome to the ${process.env.POKEDEX_NAME} Pok&eacute;dex API!</h1>`);
 });
+
+// utilisation de la variable d'env avec une varibale par defaut
+
+fastify.get(HEALTHCHECK_ENDPOINT, {logLevel : 'silent'}, (request, reply) => {
+    reply
+        .send({status: "Healthy"});
+});
+// pouvoir etre utiliser dans le deploiement
+// ce endpojt on va egalement le rendre configurable
 
 fastify.register(pokemons);
 
